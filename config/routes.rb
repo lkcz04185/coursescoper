09d146217a5425842749660d60682abc7a23ba38
lkcz04185 <lkcz04185@gmail.com>
2016-11-16 14:40:50 +0000

    courses listing

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
+  resources :courses, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
