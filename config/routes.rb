2e9e14be17e084c1b39420bbf7d6b2f5fb4d8b97
lkcz04185 <lkcz04185@gmail.com>
2016-11-24 12:30:09 +0000

    sections accomodated

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   # get 'static/pages'
 
   root 'static#index'
-  namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
-  end
   resources :courses, only: [:index, :show]
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
+  end
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
