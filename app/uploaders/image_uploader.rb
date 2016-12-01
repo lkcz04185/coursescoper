05bdd3ccdb8180ed62cd83f9a3da3213b4d2e26d
lkcz04185 <lkcz04185@gmail.com>
2016-12-01 12:00:43 +0000

    MiniMagick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -24,6 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
+  process resize_to_fit: [900, 350, gravity='Center']
   # process scale: [200, 300]
   #
   # def scale(width, height)
