5745ee01758d95c2eb98e12598a2f725d25b228e
lkcz04185 <lkcz04185@gmail.com>
2016-12-13 15:29:27 +0000

    section ordering

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,8 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
+
   def new
         @section =  Section.new
   end
@@ -8,6 +10,10 @@ class Instructor::SectionsController < ApplicationController
   def create
     @section = @current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
+  end
+
+  def update
+    current_section.update_attributes(section_params)
   end
 
   private
@@ -18,12 +24,27 @@ class Instructor::SectionsController < ApplicationController
     end
   end
 
-  helper_method :current_course
-  def current_course
-    @current_course ||= Course.find(params[:course_id])
+   def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render text: 'Unauthorized', status: :unauthorized
+    end
   end
 
+  helper_method :current_course
+  def current_course
+    if params [:course_id]
+    @current_course ||= Course.find(params[:course_id])
+    else
+    current_section.course
+    end
+  end
+
+  helper_method :current_section
+    def current_section
+      @current_section ||= Section.find(params[:id])
+    end
+
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
